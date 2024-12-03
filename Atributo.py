f9a7fe2a4f3b6199518cf10721d4bbef39ebe54d
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2024-12-03 20:13:54 +0000

    Atributo

--- Atributo.py
+++ Atributo.py
@@ -1,22 +1,23 @@
-class Pessoa:
+class Pessoa:      #parâmetro ↓
     def __init__(self, v_construtor):
                                 #Atributos  ↓
         self.nome =             'Luis'
-        self.sobrenome =        'Santana'
+        self.sobrenome =        'Santana'               # <- Valores de dentro da classe
         self.lista_nomes =      ['João', 'Maria']
         self.dicionario =       {'nome' : 'Luis', 'sobrenome': 'Santana'}
-        self.aluno =            v_construtor
+        self.aluno =            v_construtor #Atributo criado apartir do parâmetro
 
+#Os atributos podem receber vários tipos de valores com origem de dentro ou fora da classe.
 
     def unir_nomes(self):
-        self.nome_completo = self.nome + self.sobrenome
+        self.nome_completo = self.nome +   self.sobrenome
         print(f"O nome completo é {self.nome_completo}")
         print(f"O nome completo é {self.lista_nomes}")
         print(f"O nome completo é {self.dicionario}")
         print(f"O nome completo é {self.aluno}")
 
-
-v_construtor = 'Luisinho'
+#Valor passado para o construtor
+v_construtor = 'Luisinho' #<- Valor fora da classe
 
 pessoa = Pessoa(v_construtor)
 
