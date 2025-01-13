a597132f007ce315560cbcabf33dae1fe45046d3
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2025-01-13 19:23:56 +0000

    Treinamento

--- Treinamento /Treinin 10.py
+++ Treinamento /Treinin 10.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 
 class ControleXbox:
     def __init__(self, valor_cor, largura, comprimento, pronfundidade):
@@ -8,6 +8,7 @@ class ControleXbox:
         self.comprimento = comprimento 
         self.profundidade = pronfundidade
 
+    
 
     def analogico_game(self, analogico):
         if analogico == "frente":
@@ -21,26 +22,42 @@ class ControleXbox:
         else: 
             pass
     
-    def controlar_personagem(self, botao, botao2):
-        if botao == "A":
-            print("Soco bola de Fogo!!")
-        elif botao == "Y":
-            print("Ataque especial")
-        elif botao == "X":
-            print("Carregar poder")
-        elif botao == "B":
-            print("Defesa")
-        elif  botao == "A" and botao2 == "X":
+    def controlar_personagem(self, botao, botao2, botao3, botao4):
+        if botao == "A" and botao2 == "Y":
+            print("Soco bola de Fogo")
+        elif botao3 == "X" and botao4 == "B":
+            print("Aura suprema")
+        elif botao == "A":
             print("Ataque Normal")
-        elif botao == "X" or botao2 == "B":
-            print("Upar defesa")
-        elif botao2 == "":
-            pass
+        elif botao2 == "Y":
+            print("Carregar Xi")
+        elif botao3 == "X":
+            print("Bola flamejante")
+        elif botao4 == "B":
+            print("Escudo")
 
+    def menu_jogo(self, menu):
+        match(menu):
+            case "S":
+                print("Jogo pausado")
+                time.sleep(6)
+                os.system("cls || clear")
+            case "N":
+                print("Então bo de gameplay HAHAHAHA")                        
+            case _:
+                print("Opção desconhecida")
+                
+def cab():
+    os.system("cls || clear")
+
+
+cab()
+menu = input("Você deseja pausar o jogo(S/N)?")
+controle_game = ControleXbox("Preto", "6cm", "10m", "4cm")
 
 os.system("cls || clear")
+time.sleep(3)
+controle_game.menu_jogo(menu)
+controle_game.analogico_game("frente")
+controle_game.controlar_personagem("","","X","B")
 
-
-controle_game = ControleXbox("Preto", "6cm", "10m", "4cm")
-controle_game.analogico_game("direita")
-controle_game.controlar_personagem("X","B")
