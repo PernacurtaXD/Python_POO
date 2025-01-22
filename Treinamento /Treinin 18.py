30d21b5357bb78d205dfb24ac4c3669ecda25030
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2025-01-22 23:39:01 +0000

    Treinamento

--- Treinamento /Treinin 18.py
+++ Treinamento /Treinin 18.py
@@ -1,23 +1,40 @@
 import os 
 
-
-QUANT = 3
 notas = []
-soma = 0
+QUANT = 3
+media = 0
+
+
+os.system("cls || clear")
 
 class Boletim:
-    def __init__(self, nota, media, nome, dataNascimento, unidade):
+    def __init__(self, nota, nome, dataNascimento):
         self.nota = nota
-        self.media = media 
         self.nome = nome
         self.datNascimento = dataNascimento
-        self.unidade = unidade
+        self.soma = 0
+        self.media = 0
+        self.contador = 0
 
-    def calculo_notas(self):
+    def calculo_notas(self): 
         for notas_aluno in notas:
-           soma+=notas_aluno
+           self.soma+=notas_aluno
+           self.contador = self.contador + 1     
 
+        self.media = self.soma / self.contador
+
+    def __str__(self):
+        return f"Média: {self.media}"
+
+
+nome_aluno = input("Qual o nome completo do aluno: ")
+datNasc_aluno = input("Qual é a data de Nascimento do aluno: ")
 
 for i in range(QUANT):
-    nota = float(input(f"Digite sua {i}ª nota:"))
+    nota = float(input(f"Digite sua {i+1}ª nota:"))
     notas.append(nota)
+
+boletim = Boletim(notas, nome_aluno, datNasc_aluno, "3")
+
+boletim.calculo_notas()
+print(boletim)
