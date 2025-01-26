859c6aa587aa7e17be11f40b2aaf55b8f7ed1c79
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2025-01-26 17:40:54 +0000

    Treinamento

---
+++ Treinamento /Treinin 19.py
@@ -0,0 +1,6 @@
+import os 
+
+print("1- Janeiro\n2- Fevereiro\n3-Março")
+print("4- Abril\n5- Maio\n6- Junho")
+print("7- Julho\n8- Agosto\n9- Setembro")
+print("10- Outubro\n11- Novembro\n10- Dezembro")
